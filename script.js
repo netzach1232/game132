f9d120dfabf57e4e5008d2d53412d1ad3ac5b402
netzach1232 <netzach3020@gmail.com>
2025-02-13 08:28:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let totalWinnings = 0;
 
 async function loadResults() {
     try {
-        const response = await fetch('./lottery.csv'); // קריאה מהתיקייה המקומית
+        const response = await fetch('../lottery.csv'); // קריאה מהתיקייה המקומית
         if (!response.ok) throw new Error('בעיה בטעינת הקובץ');
         
         const data = await response.text();
